4886c06bf9d790d578d96a54a05f202b1d07207b
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-30 23:40:41 +0500

    added content section

---
+++ src/components/layout/content.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="content">
+    <div class="section--one">
+      <div class="des">
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  </main>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,12 +1,14 @@
 <template>
   <HeaderVue />
+  <ContentVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderVue from "./header.vue";
+import ContentVue from "./content.vue";
 
 export default defineComponent({
-  components: { HeaderVue },
+  components: { HeaderVue, ContentVue },
   setup() {
     return {};
   },
