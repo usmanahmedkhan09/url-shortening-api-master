b9fbfa89f9d5a706ff645e6d4c8ff38fe2776d05
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-01 18:06:34 +0500

    changes in css

--- src/components/urlshortencomponent.vue
+++ src/components/urlshortencomponent.vue
@@ -4,6 +4,20 @@
       <input type="text" />
       <button class="button">Shorten it!</button>
     </div>
-    <div class="urlContainer"></div>
+    <div class="urlContainer">
+      <p class="longurl">https://www.fontendmentor.io</p>
+      <p class="shorturl">https://rel.ink/k4IKyk</p>
+      <button>Copy</button>
+    </div>
+    <div class="urlContainer">
+      <p class="longurl">https://www.fontendmentor.io</p>
+      <p class="shorturl">https://rel.ink/k4IKyk</p>
+      <button class="copied">Copied!</button>
+    </div>
+    <div class="urlContainer">
+      <p class="longurl">https://www.fontendmentor.io</p>
+      <p class="shorturl">https://rel.ink/k4IKyk</p>
+      <button>Copy</button>
+    </div>
   </div>
 </template>
