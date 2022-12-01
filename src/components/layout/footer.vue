a43638636e479387ea81b788a2dd609a802faf43
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-01 22:53:10 +0500

    footer component added

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <div class="headSection">
+      <h2>Boost your links today</h2>
+      <button class="">Get Started</button>
+    </div>
+    <div class="body"></div>
+  </footer>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,14 +1,16 @@
 <template>
   <HeaderVue />
   <ContentVue />
+  <FooterVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderVue from "./header.vue";
 import ContentVue from "./content.vue";
+import FooterVue from "./footer.vue";
 
 export default defineComponent({
-  components: { HeaderVue, ContentVue },
+  components: { HeaderVue, ContentVue, FooterVue },
   setup() {
     return {};
   },
