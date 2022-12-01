e32b2fcbc65ae89a25ec08fd1c3d1899e977c5b6
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-01 23:32:00 +0500

    footer completed

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -4,15 +4,50 @@
       <h2>Boost your links today</h2>
       <button class="">Get Started</button>
     </div>
-    <div class="body"></div>
+    <div class="body">
+      <LogoVue />
+      <ul>
+        <li>Features</li>
+        <li>Link Shortening</li>
+        <li>Brand links</li>
+        <li>Analytics</li>
+      </ul>
+      <ul>
+        <li>Resources</li>
+        <li>Blog</li>
+        <li>Developers</li>
+        <li>Support</li>
+      </ul>
+      <ul>
+        <li>Company</li>
+        <li>About</li>
+        <li>Our Team</li>
+        <li>Careers</li>
+        <li>Contact</li>
+      </ul>
+      <div class="icons_wrapper" v-for="(icon, index) in icons" :key="index">
+        <img :src="getImageUrl(icon)" alt="" />
+      </div>
+    </div>
   </footer>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import LogoVue from "../icons/logo.vue";
 
 export default defineComponent({
+  components: { LogoVue },
   setup() {
-    return {};
+    const icons = ref([
+      "icon-facebook",
+      "icon-twitter",
+      "icon-pinterest",
+      "icon-instagram",
+    ]);
+    const getImageUrl = (name: any) => {
+      return new URL(`../../assets/images/${name}.svg`, import.meta.url).href;
+    };
+    return { icons, getImageUrl };
   },
 });
 </script>
